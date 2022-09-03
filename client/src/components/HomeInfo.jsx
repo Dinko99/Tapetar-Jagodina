6fef8f1951e6a69696d5865c6bfc39995060aa17
Dinko99 <dinkodin99@gmail.com>
2022-09-04 01:01:36 +0200

    Fixed footer responsivness, added some minor content changes

--- client/src/components/HomeInfo.jsx
+++ client/src/components/HomeInfo.jsx
@@ -1,32 +1,39 @@
 import './HomeInfo.scss';
 import { AiFillSchedule, AiFillEuroCircle } from 'react-icons/ai';
 import { IoIosTime } from 'react-icons/io';
+import { Link } from 'react-router-dom';
 
 const HomeInfo = () => {
   return (
     <section className='home-info'>
       <div className='home-info-box'>
-        <IoIosTime />
-        <h1>Radno Vreme</h1>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio ullam
-          ut maiores obcaecati, autem saepe a id molestias minima. Assumenda.
-        </p>
-      </div>
-      <div className='home-info-box'>
         <AiFillEuroCircle />
         <h1>Najbolje Cene</h1>
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio ullam
-          ut maiores obcaecati, autem saepe a id molestias minima. Assumenda.
+          Usluge dostupne svakome.
+          <br />
+          Naše cene su najpristupačnije u Jagodini i regionu, dođite i lično se
+          uverite.
+        </p>
+      </div>
+      <div className='home-info-box'>
+        <IoIosTime />
+        <h1>Radno Vreme</h1>
+        <p>
+          Ponedeljak - Petak: 18:00-21:00
+          <br />
+          Subota - Nedelja: 11:00-21:00
+          <br />
+          Posetite nas u lokalu tokom našeg radnog vremena ili nas{' '}
+          <Link to='/contact'>kontaktirajte.</Link>
         </p>
       </div>
       <div className='home-info-box'>
         <AiFillSchedule />
         <h1>Dogovoreni Rokovi</h1>
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio ullam
-          ut maiores obcaecati, autem saepe a id molestias minima. Assumenda.
+          Uvek se trudimo da završimo posao u najkraćem mogućem roku i nikad ne
+          odstupamo od dogovorenog roka.
         </p>
       </div>
     </section>
--- client/src/components/InfoMenu.jsx
+++ client/src/components/InfoMenu.jsx
@@ -16,7 +16,11 @@ const InfoMenu = () => {
           <a href='#'>
             <FaInstagram />
           </a>
-          <a href='#'>
+          <a
+            href='https://www.google.com/maps/place/%D0%A2%D1%80%D0%B3+%D0%9C%D0%B8%D0%BB%D0%B0%D0%BD%D0%B0+%D0%9F%D0%B0%D0%BD%D1%82%D0%B8%D1%9B%D0%B0/@43.9786341,21.2469899,17z/data=!3m1!4b1!4m5!3m4!1s0x4756c4693c0de859:0x776c241fb6d5d2a9!8m2!3d43.9786303!4d21.2491786'
+            target='_blank'
+            rel='noreferrer'
+          >
             <FaMapMarkerAlt />
           </a>
         </div>
