642dc72e53fb6cd9ba38b742c03e61dfa79a906a
Dinko99 <dinkodin99@gmail.com>
2022-09-05 00:05:52 +0200

    Finished Services page

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import './Services.scss';
+import Furniture from '../assets/images/furniture.jpg';
+import Restaurant from '../assets/images/restaurant.jpg';
+import Bus from '../assets/images/bus.jpg';
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className='services'>
+      <h1>Usluge Koje Nudimo</h1>
+      <div className='services-image-container'>
+        <div className='image-box'>
+          <img src={Furniture} alt='Furniture' />
+          <p>
+            Presvlačenje dvoseda, troseda, fotelja, ugaonih garnitura, stolica,
+            taburea...
+          </p>
+        </div>
+        <div className='image-box'>
+          <img src={Restaurant} alt='Restaurant' />
+          <p>
+            Presvlačenje nameštaja u restoranima, kafićima, radnim objektima...
+          </p>
+        </div>
+        <div className='image-box'>
+          <img src={Bus} alt='Bus' />
+          <p>Presvlačenje sedišta u autobusima, vozovima, kamionima...</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
